45af681bee738574ed036806abf3d07e3c59b77c
Nick <shumadabest@gmail.com>
2020-10-13 20:13:49 +0200

    EyeDetection

--- CreateDataset.py
+++ CreateDataset.py
@@ -2,9 +2,6 @@ import os
 import cv2
 import numpy as np
 
-IMG_WIDTH = 50
-IMG_HEIGHT = 50
-
 
 def create_dataset():
 
@@ -16,7 +13,7 @@ def create_dataset():
         for file in os.listdir(os.path.join(data, directory)):
             image_path = os.path.join(data, directory,  file)
             image = cv2.imread(image_path, 0)
-            image = cv2.resize(image, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_AREA)
+            image = cv2.resize(image, (50, 50), interpolation=cv2.INTER_AREA)
             image = np.array(image)
             image = image.astype('float32')
             image /= 255
